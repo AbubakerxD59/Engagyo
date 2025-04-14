c84905ee0128fd9e3f3470a07515ae5dde5b4f16
Muhammad Abubaker <abmasood5900@gmail.com>
2025-04-14 17:03:51 +0500

    minor changes

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\FrontEnd;
+namespace app\Http\Controllers\FrontEnd;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
--- routes/frontend.php
+++ routes/frontend.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\FrontEnd\AuthController;
-use App\Http\Controllers\FrontEnd\FrontendController;
+use app\Http\Controllers\FrontEnd\AuthController;
+use app\Http\Controllers\FrontEnd\FrontendController;
 use App\Http\Controllers\User\AccountsController;
 use Illuminate\Support\Facades\Route;
 
