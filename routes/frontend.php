60ac95ce102bb684675a9b211ada3cc0a20a1ba9
Muhammad Abubaker <abmasood5900@gmail.com>
2025-04-14 15:05:34 +0500

    minor chanes

--- routes/frontend.php
+++ routes/frontend.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\Frontend\AuthController;
-use App\Http\Controllers\Frontend\FrontendController;
+use App\Http\Controllers\FrontEnd\AuthController;
+use App\Http\Controllers\FrontEnd\FrontendController;
 use App\Http\Controllers\User\AccountsController;
 use Illuminate\Support\Facades\Route;
 
