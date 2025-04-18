1ef9d0dc0618f8833ce34507de64e470fe0fdc1d
Muhammad Abubaker <abmasood5900@gmail.com>
2025-04-18 19:00:41 +0500

    minor changes

--- routes/redirect.php
+++ routes/redirect.php
@@ -5,5 +5,5 @@ use App\Http\Controllers\FrontEnd\PinterestController;
 
 Route::name("pinterest.")->controller(PinterestController::class)->group(function () {
     Route::get("pinterest-callback", "pinterestCallback")->name("callback");
-    Route::pot("add-board", "addBoard")->name("addBoard");
+    Route::post("add-board", "addBoard")->name("addBoard");
 });
