34d029ea1653de8000242886785d0ef6685f914d
Muhammad Abubaker <abmasood5900@gmail.com>
2025-04-23 13:25:43 +0500

    minro changes

--- resources/views/user/accounts/index.blade.php
+++ resources/views/user/accounts/index.blade.php
@@ -48,10 +48,6 @@
                                     <div>
                                         <div class="account_name">{{ $pin->username }}</div>
                                     </div>
-                                    <a href="{{ route('panel.accounts.pinterest.delete', $pin->id) }}"
-                                        class="btn btn-outline-primary">
-                                        Delete
-                                    </a>
                                 </article>
                             @endforeach
                         </div>
