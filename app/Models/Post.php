39dfea04b74304ecb1f87bf083bb5ba7c53b86d5
Muhammad Abubaker <abmasood5900@gmail.com>
2025-04-24 16:36:22 +0500

    minor changes

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -32,7 +32,7 @@ class Post extends Model
 
     public function board()
     {
-        return $this->belongsTo(Board::class, 'account_id', 'id');
+        return $this->belongsTo(Board::class, 'account_id', 'board_id');
     }
 
     public function domain()
