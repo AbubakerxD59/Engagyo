3789fd209855c83b9ca7bf175628dbb8d0a6d079
Muhammad Abubaker <abmasood5900@gmail.com>
2025-04-25 15:26:06 +0500

    minor changes

--- resources/views/user/automation/post.blade.php
+++ resources/views/user/automation/post.blade.php
@@ -1,10 +1,19 @@
 <div class="card">
     <div class="card-header with-border clearfix">
         <div class="card-title">
-           <img src="{{$post->image}}" alt="">
+            <a href="{{ $post->url }}" target="_blank">
+                <img src="{{ $post->image }}" alt="{{ no_image() }}" width="120px">
+            </a>
         </div>
     </div>
     <div class="card-body">
-
+        <div class="row">
+            <div class="col-md-4 form-group">
+                <p>{{ $post->title }}</p>
+            </div>
+            <div class="col-md-4 form-group">
+                <p>{{ $post->title }}</p>
+            </div>
+        </div>
     </div>
 </div>
