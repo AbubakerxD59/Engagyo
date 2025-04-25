92ba3a0c752658c0f545eae03eaf03bc767cc555
Muhammad Abubaker <abmasood5900@gmail.com>
2025-04-26 02:25:11 +0500

    minor changes

--- app/Http/Controllers/User/AutomationController.php
+++ app/Http/Controllers/User/AutomationController.php
@@ -251,7 +251,7 @@ class AutomationController extends Controller
                                 "title" => $post->title,
                                 "link" => $post->url,
                                 "board_id" => $post->account_id,
-                                "media_source" => array(
+                                "media_source" => (object) array(
                                     "source_type" => "image_url",
                                     "url" => $post->image
                                 )
