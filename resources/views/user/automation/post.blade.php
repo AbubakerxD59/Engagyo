d90fd1b6f83f042b691fbfc4953a0d66a2bf112f
Muhammad Abubaker <abmasood5900@gmail.com>
2025-04-25 15:39:36 +0500

    minor changes

--- resources/views/user/automation/post.blade.php
+++ resources/views/user/automation/post.blade.php
@@ -1,6 +1,6 @@
 <div class="d-flex justify-content-center">
     <div class="card col-md-6">
-        <div class="card-header with-border clearfix">
+        <div class="card-header with-border clearfix d-flex justify-content-center">
             <div class="card-title">
                 <a href="{{ $post->url }}" target="_blank">
                     <img src="{{ $post->image }}" alt="{{ no_image() }}" width="150px">
@@ -13,8 +13,8 @@
                     <p>{{ $post->title }}</p>
                 </div>
                 <div class="form-group font-weight-bold">
-                    <span>
-                        <i class="fa fa-clock"></i>
+                    <span class="row">
+                        <i class="fa fa-clock m-1"></i>
                         <p>{{ date('Y-m-d H:i A', strtotime($post->publish_date)) }}</p>
                     </span>
                 </div>
