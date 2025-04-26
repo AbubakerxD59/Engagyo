49b9de5989fa02d846d98d9f44604f08da97b315
Muhammad Abubaker <abmasood5900@gmail.com>
2025-04-26 14:36:53 +0500

    minor changes

--- resources/views/user/automation/action.blade.php
+++ resources/views/user/automation/action.blade.php
@@ -1,10 +1,12 @@
 <div class="d-flex">
-    <div>
-        <button class="btn btn-outline-success btn-sm publish-post" data-id="{{ $post->id }}"
-            data-type="{{ $post->type }}">
-            Publish
-        </button>
-    </div>
+    @if ($post->status != 1)
+        <div>
+            <button class="btn btn-outline-success btn-sm publish-post" data-id="{{ $post->id }}"
+                data-type="{{ $post->type }}">
+                Publish
+            </button>
+        </div>
+    @endif
     <div>
         <a class="btn btn-outline-primary btn-sm post_edit" data-toggle="modal" data-target="#editPostModal"
             data-body="{{ $post }}">
