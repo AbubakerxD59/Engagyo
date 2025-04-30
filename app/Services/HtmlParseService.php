98c5bfc66dfafbd0de659adb5c9a417613739d91
Muhammad Abubaker <abmasood5900@gmail.com>
2025-04-30 15:15:00 +0500

    minor changes

--- app/Services/HtmlParseService.php
+++ app/Services/HtmlParseService.php
@@ -136,7 +136,10 @@ class HtmlParseService
         foreach ($tags as $tag) {
             if ($this->get_aspect_ratio($tag)) {
                 $pin_image = true;
-                $image = $tag->hasAttribute('data-lazy-src') ? $tag->getAttribute('data-lazy-src') : $tag->getAttribute('src');
+                $image = $tag->getAttribute('src');
+				if ($tag->hasAttribute('data-lazy-src')) {
+					$image = $tag->getAttribute("data-lazy-src");
+				}
                 $pinterest_image = $image;
                 break;
             }
