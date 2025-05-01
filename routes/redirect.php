d838ff57f4bded933db19e678e29ffba28aca4f1
Muhammad Abubaker <abmasood5900@gmail.com>
2025-05-01 14:09:24 +0500

    facebook callbacks

--- routes/redirect.php
+++ routes/redirect.php
@@ -1,9 +1,15 @@
 <?php
 
+use App\Http\Controllers\FrontEnd\FacebookController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FrontEnd\PinterestController;
 
 Route::name("pinterest.")->controller(PinterestController::class)->group(function () {
     Route::get("pinterest-callback", "pinterestCallback")->name("callback");
     Route::post("add-board", "addBoard")->name("addBoard");
+});
+
+Route::name("facebook.")->controller(FacebookController::class)->group(function(){
+    Route::get("facebook-delete-callback", "deleteCallback")->name("deleteCallback");
+    Route::get("facebook-callback", "facebookCallback")->name("callback");
 });
