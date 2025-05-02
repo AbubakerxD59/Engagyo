6dd2f20e98e55244f2eda33191503a8e0eadad91
Muhammad Abubaker <abmasood5900@gmail.com>
2025-05-02 15:51:00 +0500

    minor changes

--- app/Services/FacebookService.php
+++ app/Services/FacebookService.php
@@ -30,6 +30,7 @@ class FacebookService
 
     public function getAccessToken()
     {
+        dd($_GET, request()->all());
         $access_token = $this->helper->getAccessToken();
         dd($access_token);
         try {
