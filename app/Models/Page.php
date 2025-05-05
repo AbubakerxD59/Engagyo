397159de8191197d73d67ff8e5fcf22c6bb43201
Muhammad Abubaker <abmasood5900@gmail.com>
2025-05-05 20:03:08 +0500

    minor changes

--- app/Models/Page.php
+++ app/Models/Page.php
@@ -17,24 +17,38 @@ class Page extends Model
         "status",
     ];
 
-    public function user(){
+    public function user()
+    {
         return $this->belongsTo(User::class, 'user_id', 'id');
     }
 
-    public function facebook(){
+    public function facebook()
+    {
         return $this->belongsTo(Facebook::class, 'fb_id', 'fb_id');
     }
 
-    public function posts(){
+    public function posts()
+    {
         return $this->hasMany(Post::class, 'account_id', 'fb_id');
     }
 
-    public function domains(){
+    public function domains()
+    {
         return $this->hasMany(Domain::class, 'account_id', 'fb_id');
     }
 
     public function scopeConnected($query, $search)
     {
         $query->where('user_id', $search['user_id'])->where('fb_id', $search['fb_id'])->where('page_id', $search['page_id']);
+    }
+
+    public function scopeSearch($query, $search)
+    {
+        $query->where('pin_id', $search)->orWhere('board_id', $search)->orWhere('name', $search);
+    }
+
+    public function scopeUserSearch($query, $id)
+    {
+        $query->where('user_id', $id);
     }
 }
