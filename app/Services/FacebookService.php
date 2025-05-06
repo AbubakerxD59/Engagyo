5e1161b64fd6c001a0ba0150cc6cd93cf7684dc0
Muhammad Abubaker <abmasood5900@gmail.com>
2025-05-06 18:38:47 +0500

    minor changes

--- app/Services/FacebookService.php
+++ app/Services/FacebookService.php
@@ -41,7 +41,7 @@ class FacebookService
             "redirect_uri" => route("facebook.callback"),
             "code" => request('code'),
         );
-        $access_token = $this->facebook->get('/oauth/access_token/?' . http_build_query($params));
+        $access_token = $this->facebook->sendRequest('GET', '/oauth/access_token/?' . http_build_query($params));
         dd($access_token);
         try {
             $access_token = $this->helper->getAccessToken();
