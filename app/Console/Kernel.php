8859bd708ac8159e8a5507e874ad36a1dd747bdc
Muhammad Abubaker <abmasood5900@gmail.com>
2025-05-08 16:17:29 +0500

    minor changes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,8 +16,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('rss:feed')->everyMinute();
-        // $schedule->command('rss:feed')->dailyAt('03:58');
+        $schedule->command('rss:feed')->everyFourHours();
     }
 
     /**
