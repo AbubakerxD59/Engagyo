e5cd717d3bf778a63ddd2c72bfbe757c1ecd7866
Muhammad Abubaker <abmasood5900@gmail.com>
2025-05-13 00:45:34 +0500

    minor changes

--- app/Http/Controllers/User/AutomationController.php
+++ app/Http/Controllers/User/AutomationController.php
@@ -87,7 +87,7 @@ class AutomationController extends Controller
             $posts = $posts->where("status", $status);
         }
         $totalRecordswithFilter = clone $posts;
-        $scheduledTill = $this->post->userSearch($user->id)->scheduledTill($search, $type, $data["account"], $domain, $status);
+        $scheduledTill = $this->post->scheduledTill($search, $type, $data["account"], $domain, $status);
         $posts = $posts->orderBy('publish_date', $order);
         /*Set limit offset */
         $posts = $posts->offset(intval($data['start']))->limit(intval($data['length']));
