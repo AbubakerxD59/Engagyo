a01c413712416bae164a7dad97cbe125f0c2259a
Muhammad Abubaker <abmasood5900@gmail.com>
2025-05-13 18:26:59 +0500

    minor changes

--- app/Services/HtmlParseService.php
+++ app/Services/HtmlParseService.php
@@ -154,7 +154,7 @@ class HtmlParseService
             $width = $image->getAttribute('width') ? (float) $image->getAttribute('width') : 0;
             if (empty($height) || empty($width)) {
                 $dimensions = $this->getImageDimensionsFromUrl($image->getAttribute('src'));
-                dd($dimensions);
+                dd($dimensions, $image->getAttribute("src"));
                 if (isset($dimensions["width"])) {
                     $width = $dimensions["width"];
                 }
