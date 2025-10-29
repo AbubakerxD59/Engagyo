8f7c0af3679efee1505bdac52ad26a23bb7cd9b7
Muhammad Abubaker <abmasood5900@gmail.com>
2025-10-30 01:45:32 +0500

    minor changes

--- app/Services/PinterestService.php
+++ app/Services/PinterestService.php
@@ -162,7 +162,7 @@ class PinterestService
         ];
 
         // The URI is relative to the base URL configured in HttpService's constructor
-        $response = $this->client->post($this->baseUrl . 'media', ['media_type' => 'video'], $headers);
+        $response = $this->client->postJson($this->baseUrl . "media", ['media_type' => 'video'], $headers);
         info("register: " . json_encode($response));
         return $response;
     }
