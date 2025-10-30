64ff23ab7a9caa681e7087cb3dd6c5eff7d649a8
Muhammad Abubaker <abmasood5900@gmail.com>
2025-10-30 22:23:30 +0500

    Task: Video publishing for Pinterest.

--- app/Services/PinterestService.php
+++ app/Services/PinterestService.php
@@ -121,63 +121,6 @@ class PinterestService
                 $upload_video = $this->uploadVideo($post, $media_upload);
             }
         }
-        // $registerResponse = $this->client->postJson($this->baseUrl . "media", ['media_type' => 'video'], $this->header);
-        // $uploadUrl = $registerResponse['upload_url'];
-        // $uploadParameters = $registerResponse['upload_parameters'];
-        // $mediaId = $registerResponse['media_id'];
-        // if ($fileContents === false) {
-        //     info("File not found on S3");
-        // } else {
-        //     // step 2
-        //     $multipart = [];
-        //     foreach ($uploadParameters as $name => $contents) {
-        //         $multipart[$name] = $contents;
-        //     }
-        //     $multipart["file"] = new CURLFile($videoPath);
-        //     // $multipart[] = [
-        //     //     'name' => 'file', // Key required by Pinterest for the file contents
-        //     //     'contents' => Utils::tryFopen($videoPath, 'r'), // Read file content
-        //     //     'filename' => basename($videoPath),
-        //     //     'headers'  => [
-        //     //         'Content-Type' => 'video/mp4',
-        //     //     ],
-        //     // ];
-        //     dd($multipart);
-        //     $uploadResponse = $this->uploadVideoToPinterestAws($uploadUrl, $multipart);
-        //     // $uploadResponse = Http::asMultipart()->post($uploadUrl, $multipart);
-        //     // step 3
-        //     $mediaReady = false;
-        //     $maxAttempts = 10;
-        //     $attempt = 0;
-        //     while (!$mediaReady && $attempt < $maxAttempts) {
-        //         $attempt++;
-        //         sleep(2);
-        //         $statusResponse = $this->client->get($this->baseUrl . 'media/' . $mediaId, [], $this->header);
-        //         $status = $statusResponse['status'] ?? 'unknown';
-        //         info("current status: " . $statusResponse['status']);
-        //         if ($status === 'succeeded') {
-        //             $mediaReady = true;
-        //         } elseif ($status === 'failed') {
-        //             throw new \Exception("Video processing failed on Pinterest side. Status: " . $statusResponse['details']);
-        //         }
-        //         info("Pinterest Media Status for $mediaId: $status (Attempt $attempt)");
-        //     }
-        //     // step 4
-        //     if ($mediaReady) {
-        //         $pinPayload = [
-        //             "board_id" => (string) $post["board_id"],
-        //             'media_id' => $mediaId,
-        //         ];
-        //         if (!empty($post["title"])) {
-        //             $pinPayload["title"] = $post["title"];
-        //         }
-        //         info(message: json_encode($pinPayload));
-        //         $pinResponse = $this->client->postJson($this->baseUrl . "pins", $pinPayload, $this->header);
-        //         dd($pinResponse);
-        //     } else {
-        //         dd("Media upload failed");
-        //     }
-        // }
     }
     private function postIntent()
     {
@@ -256,14 +199,15 @@ class PinterestService
     {
         $payload = [
             "title" => $postData["title"],
-            "board_id" => $postData["board_id"],
+            "board_id" => (string) $postData["board_id"],
             "media_source" => [
                 "source_type" => "video_id",
-                "media_id" => $media_id
+                "media_id" => $media_id,
+                "cover_image_key_frame_time" => 1
             ]
         ];
         $response = $this->client->postJson($this->baseUrl . "pins", $payload, $this->header);
-        dd($response);
+        dd($response, $payload);
         return $response;
     }
 }
