ab3f598a36bb041964def4a2fe0ca03c933f5a23
Muhammad Abubaker <abmasood5900@gmail.com>
2025-11-18 14:01:00 +0500

    1. Link preview

--- app/Services/HtmlParseService.php
+++ app/Services/HtmlParseService.php
@@ -82,18 +82,14 @@ class HtmlParseService
     {
         $dom = new DOMDocument();
         @$dom->loadHTML($response);
-        echo '1-';
         // fetch from meta tags
         $meta_image = null;
         $metaTags = $dom->getElementsByTagName('meta');
         $ogimage = $ogimagesecure = null;
         foreach ($metaTags as $tag) {
             if ($tag->hasAttribute('property') && $tag->getAttribute('property') === 'og:image') {
-                echo 'og:image-';
                 if ($tag->hasAttribute('content')) {
-                    echo 'content-';
                     $ogimage = $tag->getAttribute('content');
-                    echo $ogimage;
                 }
             }
             if ($tag->hasAttribute('property') && $tag->getAttribute('property') === 'og:image:secure_url') {
@@ -109,8 +105,6 @@ class HtmlParseService
         } else {
             $meta_image = $ogimage;
         }
-        echo '2-';
-        echo $meta_image;
         $images = $dom->getElementsByTagName('img');
         // fetch images if pinterest channel is active 
         if ($this->pinterest_active) {
@@ -120,8 +114,6 @@ class HtmlParseService
             }
         }
         // fetch images from img tags
-        echo '3-';
-        echo $meta_image;
         if (empty($meta_image)) {
             foreach ($images as $thumbnail) {
                 if ($this->check_for($thumbnail->getAttribute('class'), 'post-thumbnail post-image')) {
@@ -131,8 +123,6 @@ class HtmlParseService
             }
         }
         // fetch images inside divs
-        echo '4-';
-        echo $meta_image;
         if (empty($meta_image)) {
             $thumbnails = $dom->getElementsByTagName('div');
             foreach ($thumbnails as $thumbnail) {
@@ -143,7 +133,6 @@ class HtmlParseService
                 }
             }
         }
-        dd($meta_image);
         return $meta_image;
     }
 
