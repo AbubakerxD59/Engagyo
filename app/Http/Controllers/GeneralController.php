8cf40d9ec3e3aec0eb06431434ccf9eddddf3989
Muhammad Abubaker <abmasood5900@gmail.com>
2025-11-19 18:43:33 +0500

    minor changes

--- app/Http/Controllers/GeneralController.php
+++ app/Http/Controllers/GeneralController.php
@@ -13,7 +13,7 @@ class GeneralController extends Controller
         $user = Auth::user();
         $accounts = $user->getAccounts();
         $check = $accounts->where("schedule_status", "active")->where("type", "!=", "pinterest")->first();
-        $pinterest_active = $check ? true : false;
+        $pinterest_active = $check ? false : true;
         $link = $request->link;
         if (!empty($link)) {
             $max_tries = 3;
