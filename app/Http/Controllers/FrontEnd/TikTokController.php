9d5f2fe13744f4f4d76586d5104fe2fb5a5822f6
Muhammad Abubaker <abmasood5900@gmail.com>
2025-12-31 20:08:57 +0500

    minor changes

--- app/Http/Controllers/FrontEnd/TikTokController.php
+++ app/Http/Controllers/FrontEnd/TikTokController.php
@@ -56,10 +56,15 @@ class TikTokController extends Controller
 
                         if ($tiktok_id) {
                             $avatar_url = isset($userInfo["meta"]["avatar_url"]) ? $userInfo["meta"]["avatar_url"] : null;
+                            $this->logService->log('tiktok', 'me_api', 'User info retrieved successfully', [
+                                "avatar_url" => $avatar_url,
+                            ]);
                             $profile_pic = !empty($avatar_url)
                                 ? saveImageFromUrl($avatar_url)
                                 : '';
-
+                            $this->logService->log('tiktok', 'me_api', 'User info retrieved successfully', [
+                                "profile_pic" => $profile_pic,
+                            ]);
                             $data = [
                                 "user_id" => $user->id,
                                 "tiktok_id" => $tiktok_id,
