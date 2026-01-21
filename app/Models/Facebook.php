173b684ca4d11b3b5b2e157c02fee21033ed406e
Muhammad Abubaker <abmasood5900@gmail.com>
2026-01-21 23:10:38 +0500

    minor changes

--- app/Models/Facebook.php
+++ app/Models/Facebook.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use App\Models\Scopes\UserScope;
+use App\Models\Scopes\TeamScope;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -75,6 +75,7 @@ class Facebook extends Model
 
     protected static function booted()
     {
-        static::addGlobalScope(new UserScope);
+        // static::addGlobalScope(new UserScope);
+        static::addGlobalScope(new TeamScope);
     }
 }
