09607dfbe2ce4ae2bd8a1b2cef1f6d09bd879343
Muhammad Abubaker <abmasood5900@gmail.com>
2026-01-31 16:14:08 +0500

    minor changes

--- app/Console/Commands/ShuffleRssPosts.php
+++ app/Console/Commands/ShuffleRssPosts.php
@@ -31,9 +31,7 @@ class ShuffleRssPosts extends Command
     public function handle()
     {
         // get posts
-        // $posts = Post::has("domain")->with(["domain", "board", "page", "tiktok"])->whereNotNull("domain_id")->isRss()->notPublished()->get();
-        // for testing
-        $posts = Post::has("domain")->with(["domain", "board", "page", "tiktok"])->where('user_id', ['4'])->whereNotNull("domain_id")->isRss()->notPublished()->get();
+        $posts = Post::has("domain")->with(["domain", "board", "page", "tiktok"])->whereNotNull("domain_id")->isRss()->notPublished()->get();
         echo 'total posts: ' . count($posts) . '<br>';
         $userBasedPosts = $posts->groupBy('user_id');
         foreach ($userBasedPosts as $user_id => $userBasedPost) {
