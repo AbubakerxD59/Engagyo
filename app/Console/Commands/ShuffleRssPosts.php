8361bddb02726e7aeb944e9a48f8c70d81cd9632
Muhammad Abubaker <abmasood5900@gmail.com>
2026-02-02 16:34:57 +0500

    minor changes

--- app/Console/Commands/ShuffleRssPosts.php
+++ app/Console/Commands/ShuffleRssPosts.php
@@ -33,46 +33,44 @@ class ShuffleRssPosts extends Command
         // get posts
         $posts = Post::with(["domain", "board", "page", "tiktok"])->whereNotNull("domain_id")->isRss()->notPublished()->get();
         echo 'total posts: ' . count($posts) . '<br>';
-        $userBasedPosts = $posts->groupBy('user_id');
-        foreach ($userBasedPosts as $user_id => $userBasedPost) {
-            echo 'user_id: ' . $user_id . '<br>';
-            $socialTypeBasedPosts = $userBasedPost->groupBy('social_type');
-            foreach ($socialTypeBasedPosts as $social_type => $socialTypeBasedPost) {
-                echo 'social_type: ' . $social_type . '<br>';
-                $accountBasedPosts = $socialTypeBasedPost->groupBy('account_id');
-                foreach ($accountBasedPosts as $account_id => $accountBasedPost) {
-                    echo 'account_id: ' . $account_id . '<br>';
-                    echo 'total_posts_for_account: ' . count($accountBasedPost) . '<br>';
-                    if ($social_type == "facebook") {
-                        $account = Page::find($account_id);
-                    }
-                    if ($social_type == "pinterest") {
-                        $account = Board::find($account_id);
-                    }
-                    if ($social_type == "tiktok") {
-                        $account = Tiktok::find($account_id);
-                    }
-                    $shuffle = $account ? $account->shuffle : false;
-                    echo 'shuffle: ' . $shuffle . '<br>';
-                    if (!$shuffle) {
-                        break;
-                    }
-                    echo "shuffling posts for user_id: $user_id, account: $account->name, social_type: $social_type <br>";
-                    // Shuffle start
-                    $publisheDates = $accountBasedPost->pluck('publish_date');
-                    $shuffledDates = $publisheDates->shuffle();
-                    DB::transaction(function () use ($accountBasedPost, $shuffledDates) {
-                        $count = 0;
-                        foreach ($accountBasedPost as $index => $post) {
-                            $post->update([
-                                "publish_date" => $shuffledDates[$count],
-                            ]);
-                            $count++;
-                        }
-                    });
-
-                    // Shuffle end
+        $socialTypeBasedPosts = $posts->groupBy('social_type');
+        foreach ($socialTypeBasedPosts as $social_type => $socialTypeBasedPost) {
+            echo 'social_type: ' . $social_type . '<br>';
+            $accountBasedPosts = $socialTypeBasedPost->groupBy('account_id');
+            foreach ($accountBasedPosts as $account_id => $accountBasedPost) {
+                $post = $accountBasedPost->first();
+                $user_id = $post->user_id;
+                echo 'account_id: ' . $account_id . '<br>';
+                echo 'total_posts_for_account: ' . count($accountBasedPost) . '<br>';
+                if ($social_type == "facebook") {
+                    $account = Page::find($account_id);
                 }
+                if ($social_type == "pinterest") {
+                    $account = Board::find($account_id);
+                }
+                if ($social_type == "tiktok") {
+                    $account = Tiktok::find($account_id);
+                }
+                $shuffle = $account ? $account->shuffle : false;
+                echo 'shuffle: ' . $shuffle . '<br>';
+                if (!$shuffle) {
+                    break;
+                }
+                echo "shuffling posts for user_id: $user_id, account: $account->name, social_type: $social_type <br>";
+                // Shuffle start
+                $publisheDates = $accountBasedPost->pluck('publish_date');
+                $shuffledDates = $publisheDates->shuffle();
+                DB::transaction(function () use ($accountBasedPost, $shuffledDates) {
+                    $count = 0;
+                    foreach ($accountBasedPost as $index => $post) {
+                        $post->update([
+                            "publish_date" => $shuffledDates[$count],
+                        ]);
+                        $count++;
+                    }
+                });
+
+                // Shuffle end
             }
         }
     }
