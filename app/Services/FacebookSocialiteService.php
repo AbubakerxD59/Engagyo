1fc4b8a52506066aa48988a3ea294eed3a200a63
Muhammad Abubaker <abmasood5900@gmail.com>
2026-02-22 23:38:02 +0500

    Socialite implementation start

--- app/Services/FacebookSocialiteService.php
+++ app/Services/FacebookSocialiteService.php
@@ -8,37 +8,38 @@ class FacebookSocialiteService
 {
     /**
      * Facebook OAuth scopes for full page management and insights.
+     * Ordered by dependency: base permissions first, then permissions that depend on them.
+     * (public_profile, email, pages_show_list have no deps; others follow Facebook's dependency chain)
      */
     protected array $scopes = [
-        'business_management',
+        'public_profile',
         'email',
-        'pages_manage_engagement',
-        'pages_manage_metadata',
-        'pages_manage_posts',
+        'pages_show_list',
         'pages_read_engagement',
         'pages_read_user_content',
-        'pages_show_list',
-        'public_profile',
+        'pages_manage_metadata',
         'read_insights',
+        'pages_manage_posts',
+        'pages_manage_engagement',
+        'business_management',
     ];
 
     /**
      * Redirect the user to Facebook for authentication.
      *
+     * Note: Do not pass config_id here. When config_id (Facebook Login for Business) is used,
+     * it overrides the scope parameter and only requests permissions defined in that Meta
+     * configuration. To request our full permission set (including pages_show_list for
+     * connecting business pages), we rely on the scope parameter alone.
+     *
      * @return \Symfony\Component\HttpFoundation\RedirectResponse
      */
     public function redirect()
     {
-        $driver = Socialite::driver('facebook')
+        return Socialite::driver('facebook')
             ->scopes($this->scopes)
-            ->redirectUrl(route('facebook.callback'));
-
-        $configId = config('services.facebook.config_id') ?? env('FACEBOOK_CONFIG_ID');
-        if ($configId) {
-            $driver->with(['config_id' => $configId]);
-        }
-
-        return $driver->redirect();
+            ->redirectUrl(route('facebook.callback'))
+            ->redirect();
     }
 
     /**
