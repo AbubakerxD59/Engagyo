36bc2c49892f863447a4179ec4b587e9d8ec6860
Muhammad Abubaker <abmasood5900@gmail.com>
2026-04-11 03:01:16 +0500

    mnior changes

--- resources/views/user/schedule-new-design/assets/style.blade.php
+++ resources/views/user/schedule-new-design/assets/style.blade.php
@@ -2357,6 +2357,10 @@
         background: #010101;
     }
 
+    .queue-settings-modal-redesign .queue-settings-badge-instagram {
+        background: linear-gradient(45deg, #f09433 0%, #e6683c 25%, #dc2743 50%, #cc2366 75%, #bc1888 100%);
+    }
+
     .queue-settings-modal-redesign .queue-settings-account-info {
         display: flex;
         flex-direction: column;
