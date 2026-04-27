a9994e64268cc2464847a858c9a307662e81f5a3
Muhammad Abubaker <abmasood5900@gmail.com>
2026-04-27 19:51:36 +0500

    minor changes

--- database/seeders/MenuSeeder.php
+++ database/seeders/MenuSeeder.php
@@ -17,7 +17,7 @@ class MenuSeeder extends Seeder
             ["id" => "2", "name" => "Automation", "icon" => "fas fa-rss", "route" => "panel.automation", "display_order" => "2"],
             ["id" => "3", "name" => "Api Posts", "icon" => "fas fa-code", "route" => "panel.api-posts", "display_order" => "3"],
             ["id" => "4", "name" => "Analytics", "icon" => "fas fa-chart-line", "route" => "panel.analytics", "display_order" => "4"],
-            ["  id" => "5", "name" => "Teams", "icon" => "fas fa-users", "route" => "panel.team-members.index", "display_order" => "5"],
+            ["id" => "5", "name" => "Teams", "icon" => "fas fa-users", "route" => "panel.team-members.index", "display_order" => "5"],
             ["id" => "6", "name" => "Url Tracking", "icon" => "fas fa-link", "route" => "panel.url-tracking", "display_order" => "6"],
             ["id" => "7", "name" => "Link Shortener", "icon" => "fas fa-compress-alt", "route" => "panel.link-shortener", "display_order" => "7"],
             ["id" => "8", "name" => "Accounts", "icon" => "fas fa-user-circle", "route" => "panel.accounts", "display_order" => "8"],
